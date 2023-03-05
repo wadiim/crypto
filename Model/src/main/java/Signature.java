e66ca50ef02881449b84fff16aa4ad0524e3d37f
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2023-03-05 20:32:48 +0100

    Add Signature interface

---
+++ Model/src/main/java/Signature.java
@@ -0,0 +1,9 @@
+public interface Signature {
+    void generateKeys();
+    byte[] getPublicKey();
+    byte[] getPrivateKey();
+    void setKeys(byte[] publicKey, byte[] privateKey);
+
+    byte[] sign(byte[] message);
+    boolean verify(byte[] message, byte[] sign);
+}
