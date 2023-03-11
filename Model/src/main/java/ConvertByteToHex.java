1e396240aad8e1dba37e54bd30ee8933be7185ad
MateuszG2001 <mateusz.gladki1236@gmail.com>
2023-03-11 23:24:03 +0100

    Add and implements ConvertByteToHex class

---
+++ Model/src/main/java/ConvertByteToHex.java
@@ -0,0 +1,38 @@
+public class ConvertByteToHex {
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
+    /**
+     * Method that converts a hexadecimal string to a byte array.
+     *
+     * @param hexString hexadecimal string to be converted
+     * @return byte array
+     */
+    public static byte[] convertHexStringToByteArray(String hexString) {
+        int stringLength = hexString.length();
+        byte[] byteArray = new byte[stringLength / 2];
+        for (int i = 0; i < stringLength; i += 2) {
+            // Convert a pair of characters to a single byte
+            byteArray[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4)
+                    + Character.digit(hexString.charAt(i + 1), 16));
+        }
+        return byteArray;
+    }
+
+    /**
+     * Method that converts a byte array to a hexadecimal string.
+     *
+     * @param byteArray byte array to be converted
+     * @return hexadecimal string
+     */
+    public static String convertByteArrayToHexString(byte[] byteArray) {
+        char[] hexChars = new char[byteArray.length * 2];
+        for (int j = 0; j < byteArray.length; j++) {
+            int value = byteArray[j] & 0xFF;
+            // Convert a single byte to a pair of hexadecimal characters
+            hexChars[j * 2] = HEX_ARRAY[value >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[value & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
